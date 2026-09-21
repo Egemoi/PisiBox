568ae77c293512a7d95054435948cc851dab3796
Egemoi <metekurter2@gmail.com>
2026-09-21 17:58:10 +0300

    Integrate anime sci-fi fantasy movie categories

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { moviesPart1 } from "./data/movies-part1";
 import { moviesPart2 } from "./data/movies-part2";
 import { moviesPart3 } from "./data/movies-part3";
 import { moviesPart4 } from "./data/movies-part4";
+import { moviesPart5 } from "./data/movies-part5";
 
-const movies = Object.values([...moviesPart1, ...moviesPart2, ...moviesPart3, ...moviesPart4].reduce((map, item) => { map[item.title] = item; return map; }, {}));
+const movies = Object.values([...moviesPart1, ...moviesPart2, ...moviesPart3, ...moviesPart4, ...moviesPart5].reduce((map, item) => { map[item.title] = item; return map; }, {}));
 
 const categories = ["Tamamen Rastgele","Korku","Romantik","Aksiyon","Komedi","Dram","Anime","Bilim Kurgu","Fantastik","Gizem","Gerilim","Aile"];
 
